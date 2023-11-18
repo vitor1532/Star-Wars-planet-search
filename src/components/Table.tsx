c9bcf19110ac858661b5c8cf56bfc7c6f0cc13b7
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-11-18 12:54:18 -0300

    inserindo dados na tabela

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -10,15 +10,46 @@ function Table() {
     <table>
       <thead>
         <tr>
-          {planets.length > 0 && Object.keys(planets[0]).map((key) => (
-            <th key={ key }>{snakeCaseToTitleCase(key)}</th>
-          ))}
+          {planets.length > 0 && Object.keys(planets[0]).map((key) => {
+            if (key === 'residents') {
+              return null;
+            } const title = snakeCaseToTitleCase(key);
+            return <th key={ key }>{title}</th>;
+          })}
         </tr>
       </thead>
       <tbody>
-        {planets.map((planet) => (
-          <tr key={ planet.name } />
+        {planets.map(({
+          name,
+          rotation_period,
+          orbital_period,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surface_water,
+          population,
+          films,
+          created,
+          edited,
+          url }) => (
+            <tr key={ name }>
+              <td>{name}</td>
+              <td>{rotation_period}</td>
+              <td>{orbital_period}</td>
+              <td>{diameter}</td>
+              <td>{climate}</td>
+              <td>{gravity}</td>
+              <td>{terrain}</td>
+              <td>{surface_water}</td>
+              <td>{population}</td>
+              <td>{films}</td>
+              <td>{created}</td>
+              <td>{edited}</td>
+              <td>{url}</td>
+            </tr>
         ))}
+
       </tbody>
     </table>
   );
