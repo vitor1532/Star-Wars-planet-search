3503912c4641d433c9ee8fb84c834acf0c46bd38
Vitor Martins <vitomartinsalmeida@gmail.com>
2023-11-20 20:27:55 -0300

    testes iniciais

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import PlanetsProvider from '../context/PlanetsProvider';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testa a renderização de todos os componentes da aplicação', () => {
+  test('Testa se os componentes são renderizados', () => {
+    render(
+      <PlanetsProvider>
+        <App />
+      </PlanetsProvider>
+      );
+
+    const title = screen.getByText(/Projeto Star Wars - Trybe/i);
+    const nameFilter = screen.getByTestId('name-filter');
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+    const buttonFilter = screen.getByTestId('button-filter');
+    const columnSortAsc = screen.getByTestId('column-sort-input-asc');
+    const columnSortDesc = screen.getByTestId('column-sort-input-desc');
+
+    
+    expect(title).toBeInTheDocument();
+    expect(nameFilter).toBeInTheDocument();
+    expect(columnFilter).toBeInTheDocument();
+    expect(comparisonFilter).toBeInTheDocument();
+    expect(valueFilter).toBeInTheDocument();
+    expect(buttonFilter).toBeInTheDocument();
+    expect(columnSortAsc).toBeInTheDocument();
+    expect(columnSortDesc).toBeInTheDocument();
+  })
+
 });
