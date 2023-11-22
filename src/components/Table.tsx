1c268df28e458405c20ab7c4293ff62608033acf
vitor1532 <vitomartinsalmeida@gmail.com>
2023-11-22 14:58:20 -0300

    aplicando estilização na table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -7,51 +7,55 @@ function Table() {
   const { filteredPlanets } = useContext(PlanetsContext);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {filteredPlanets.length > 0 && Object.keys(filteredPlanets[0]).map((key) => {
-            if (key === 'residents') {
-              return null;
-            } const title = snakeCaseToTitleCase(key);
-            return <th data-testid="table-head" key={ key }>{title}</th>;
-          })}
-        </tr>
-      </thead>
-      <tbody>
-        {filteredPlanets.map(({
-          name,
-          rotation_period,
-          orbital_period,
-          diameter,
-          climate,
-          gravity,
-          terrain,
-          surface_water,
-          population,
-          films,
-          created,
-          edited,
-          url }) => (
-            <tr key={ name }>
-              <td data-testid="planet-name">{name}</td>
-              <td>{rotation_period}</td>
-              <td>{orbital_period}</td>
-              <td>{diameter}</td>
-              <td>{climate}</td>
-              <td>{gravity}</td>
-              <td>{terrain}</td>
-              <td>{surface_water}</td>
-              <td>{population}</td>
-              <td>{films}</td>
-              <td>{created}</td>
-              <td>{edited}</td>
-              <td>{url}</td>
-            </tr>
-        ))}
+    <div className="table-wrapper">
+      <table>
+        <thead>
+          <tr>
+            {filteredPlanets.length > 0 && Object.keys(
+              filteredPlanets[0],
+            ).map((key) => {
+              if (key === 'residents') {
+                return null;
+              } const title = snakeCaseToTitleCase(key);
+              return <th data-testid="table-head" key={ key }>{title}</th>;
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {filteredPlanets.map(({
+            name,
+            rotation_period,
+            orbital_period,
+            diameter,
+            climate,
+            gravity,
+            terrain,
+            surface_water,
+            population,
+            films,
+            created,
+            edited,
+            url }) => (
+              <tr key={ name }>
+                <td data-testid="planet-name">{name}</td>
+                <td>{rotation_period}</td>
+                <td>{orbital_period}</td>
+                <td>{diameter}</td>
+                <td>{climate}</td>
+                <td>{gravity}</td>
+                <td>{terrain}</td>
+                <td>{surface_water}</td>
+                <td>{population}</td>
+                <td>{films}</td>
+                <td>{created}</td>
+                <td>{edited}</td>
+                <td>{url}</td>
+              </tr>
+          ))}
 
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
